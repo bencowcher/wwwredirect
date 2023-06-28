26845f088387e67d9377e9bc85bed27bc1fc5ddc
Ben Cowcher <benc@punterstech.com>
2023-06-28 14:17:28 +1000

    add whitelist to domains

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/exp/slices"
 )
+
+func isWhiteListed(host string) bool {
+	env := os.Getenv("WHITELISTED_DOMAINS")
+	domains := strings.Split(env, ",")
+
+	return slices.Contains(domains, host)
+}
 
 // determines the domain name requested and returns the www redirect
 func wwwRedirect(w http.ResponseWriter, r *http.Request) {
 	// get the host
 	host := r.Host
+
 	log.Println(host)
 	// check if the host is www
-	if host[:4] != "www" {
+	if isWhiteListed(host) && !strings.HasPrefix(host, "www") {
 		// redirect to www
 		log.Println("redirecting:", "https://www."+host)
 		http.Redirect(w, r, "https://www."+host, http.StatusPermanentRedirect)
