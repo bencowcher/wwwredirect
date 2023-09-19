f738d7845cbd00d626151b3c6601603b2ec42735
Ben Cowcher <benc@punterstech.com>
2023-09-20 08:27:12 +1000

    enable the use of a k8s config map

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build -t bencowcher/wwwredirect . --platform=linux/amd64
+docker push bencowcher/wwwredirect
--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	_ "embed"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 //go:embed hosts.json
 var hosts []byte
 
-var hostmap = loadHosts()
+var hostmap = map[string]string{}
 
-func loadHosts() map[string]string {
+func loadEmbeddedHosts() map[string]string {
 	var hostmap map[string]string
 	err := json.Unmarshal(hosts, &hostmap)
 	if err != nil {
@@ -22,6 +28,25 @@ func loadHosts() map[string]string {
 	}
 
 	return hostmap
+}
+
+func loadKubernetesHosts(ns string) map[string]string {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	confmap, err := clientset.CoreV1().ConfigMaps(ns).Get(context.Background(), "hosts", metav1.GetOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return confmap.Data
 }
 
 // determines the domain name requested and returns the www redirect
@@ -44,6 +69,17 @@ func wwwRedirect(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	src := os.Getenv("APP_HOST_SOURCE") // embedded, kubernetes
+	if src == "" {
+		src = "embedded"
+	}
+
+	if src == "kubernetes" {
+		hostmap = loadKubernetesHosts(os.Getenv("APP_KUBERNETES_NAMESPACE"))
+	} else {
+		hostmap = loadEmbeddedHosts()
+	}
+
 	// create the server
 	server := http.Server{
 		Addr: "0.0.0.0:80",
